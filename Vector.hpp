1323b4a847dc32a84f019fc622e549ecec504370
Jiyoon Hur <jiyoon1156@gmail.com>
2020-11-09 15:38:29 +0900

    ADD: vector capacity functions

--- Vector.hpp
+++ Vector.hpp
@@ -119,6 +119,7 @@ namespace ft
 			T*			_ptr;
 			size_t	_capa;
 			size_t	_size;
+			Alloc		_alloc;
 		public:
 			typedef T																value_type;
 			typedef Alloc														allocator_type;
@@ -133,32 +134,123 @@ namespace ft
 			typedef std::ptrdiff_t 									difference_type;
 			typedef size_t 													size_type;
 
-			explicit Vector (const allocator_type& alloc = allocator_type());
-			explicit Vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
+			/*
+			allocate 초기화되지 않은 메모리 공간 할당해서 시작 주소 반환
+			deallocate 함수는 메모리 공간을 해제
+			construct 함수는 초기화되지 않은 공간에 요소를 저장
+			destroy 함수는 객체를 소멸
+			uninitialized_copy 함수는 멤버 함수는 아니지만 자주 사용되는 관련된 함수입니다.
+			STL의 std::copy함수와 비슷하며 입력 반복자 2개(first, last)와 순방향 반복자 1개(out)를 인자로 받습니다.
+			[first, last) 범위의 요소들을 out이 가리키는 위치에 순서대로 복사합니다.
+			그리고 이 함수는 복사가 완료된 위치의 다음 요소를 가리키는 포인터를 반환합니다.
+			즉 1~5 위치에 복사를 완료했다면 6위치를 가리키는 포인터를 반환합니다.
+			*/
+			explicit Vector (const allocator_type& alloc = allocator_type()): _ptr(nullptr), _size(0), _capa(0)
+			{
+				_ptr = _alloc.allocate(1);
+			};
+			explicit Vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()): _ptr(nullptr), _size(n), _capa(n)
+			{
+				_ptr = _alloc.allocate(n + 1);
+				for (int i = 0; i < n; ++i)
+					_alloc.construct(&_ptr[i], val);
+			};
 			template <class InputIterator>
-			Vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
-			Vector (const Vector& x);
-			~Vector(){};
-			Vector& operator= (const Vector& x);
+			Vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()): _ptr(nullptr), _size(0), _capa(0)
+			{
+				_ptr = _alloc.allocate(1);
+				assign(first, last);
+			};
+			Vector (const Vector& x): _ptr(nullptr), _size(0), _capa(0)
+			{
+				*this = x;
+			};
+			~Vector()
+			{
+				for (int i = 0; i < _size; ++i)
+					_alloc.destroy((_ptr + i));
+				_alloc.deallocate(_ptr, _capa + 1);
+			};
+			Vector& operator= (const Vector& x)
+			{
+				assing(x.begin(), x.end());
+				return (*this);
+			};
 
 			/* iterators */
-			iterator begin();
-			const_iterator begin() const;
-			iterator end();
-			const_iterator end() const;
-			reverse_iterator rbegin();
-			const_reverse_iterator rbegin() const;
-			reverse_iterator rend();
-			const_reverse_iterator rend() const;
+			iterator begin()
+			{
+				return iterator(_ptr);
+			};
+			const_iterator begin() const
+			{
+				return const_iterator(_ptr);
+			};
+			iterator end()
+			{
+				return iterator(_ptr + _size);
+			};
+			const_iterator end() const
+			{
+				return const_iterator(_ptr + _size);
+			};
+			reverse_iterator rbegin()
+			{
+				return (reverse_iterator(end()));
+			};
+			const_reverse_iterator rbegin() const
+			{
+				return (const_reverse_iterator(end()));
+			};
+			reverse_iterator rend()
+			{
+				return (reverse_iterator(begin()));
+			};
+			const_reverse_iterator rend() const
+			{
+				return (const_reverse_iterator(begin()));
+			};
 
 			/* capacity */
-			size_type size() const;
-			size_type max_size() const;
-			void resize (size_type n, value_type val = value_type());
-			size_type capacity() const;
-			bool empty() const;
-			void reserve (size_type n);
-			void shrink_to_fit();
+			size_type size() const
+			{
+				return (_size);
+			};
+			size_type max_size() const
+			{
+				return (std::numeric_limits<T>::max() / sizeof(T));
+			};
+			void resize (size_type n, value_type val = value_type())
+			{
+				while(n < _size)
+					pop_back();
+				while(_size < n)
+					push_back(val);
+			};
+			size_type capacity() const
+			{
+				return (_capa);
+			};
+			bool empty() const
+			{
+				return (_size == 0);
+			};
+			void reserve (size_type n)
+			{
+				if (n > max_size())
+					throw std::length_error("length error");
+				if (n > _capa)
+				{
+					T* newPtr;
+					newPtr = _alloc.allocate(n + 1);
+					for (size_type i = 0; i < _size; ++i)
+						_alloc.construct(&newPtr[i], _ptr[i]);
+					_ptr = newPtr;
+					_capa = n;
+				}
+				else
+					throw std::bad_alloc("bad alloc");
+			};
 
 			/* element access */
 			reference operator[] (size_type n);
@@ -191,13 +283,13 @@ namespace ft
 
 	/* non-member function overloads */
 	template <class T, class Alloc>
-	bool operator== (const List<T,Alloc>& lhs, const List<T,Alloc>& rhs)
+	bool operator== (const Vector<T,Alloc>& lhs, const Vector<T,Alloc>& rhs)
 	{
 		//사이즈로 먼저 비교하고 val 비교
 		if (lhs.size() != rhs.size())
 			return (false);
-		typename List<T, Alloc>::const_iterator	it_r = rhs.begin();
-		typename List<T, Alloc>::const_iterator	it_l = lhs.begin();
+		typename Vector<T, Alloc>::const_iterator	it_r = rhs.begin();
+		typename Vector<T, Alloc>::const_iterator	it_l = lhs.begin();
 		for (; (it_l != lhs.end()) && (it_r != rhs.end()); ++it_l, ++it_r)
 		{
 			if (*it_l != *it_r)
@@ -207,16 +299,16 @@ namespace ft
 	};
 
 	template <class T, class Alloc>
-	bool operator!= (const List<T,Alloc>& lhs, const List<T,Alloc>& rhs)
+	bool operator!= (const Vector<T,Alloc>& lhs, const Vector<T,Alloc>& rhs)
 	{
 		return (!(lhs == rhs));
 	};
 
 	template <class T, class Alloc>
-	bool operator<  (const List<T,Alloc>& lhs, const List<T,Alloc>& rhs)
+	bool operator<  (const Vector<T,Alloc>& lhs, const Vector<T,Alloc>& rhs)
 	{
-		typename List<T, Alloc>::const_iterator	it_r = rhs.begin();
-		typename List<T, Alloc>::const_iterator	it_l = lhs.begin();
+		typename Vector<T, Alloc>::const_iterator	it_r = rhs.begin();
+		typename Vector<T, Alloc>::const_iterator	it_l = lhs.begin();
 		for (; (it_l != lhs.end()) && (it_r != rhs.end()); ++it_l, ++it_r)
 		{
 			if (*it_l < *it_r)
@@ -228,25 +320,25 @@ namespace ft
 	};
 
 	template <class T, class Alloc>
-	bool operator<= (const List<T,Alloc>& lhs, const List<T,Alloc>& rhs)
+	bool operator<= (const Vector<T,Alloc>& lhs, const Vector<T,Alloc>& rhs)
 	{
 		return (!(lhs > rhs));
 	};
 
 	template <class T, class Alloc>
-	bool operator>  (const List<T,Alloc>& lhs, const List<T,Alloc>& rhs)
+	bool operator>  (const Vector<T,Alloc>& lhs, const Vector<T,Alloc>& rhs)
 	{
 		return (rhs < lhs);
 	};
 
 	template <class T, class Alloc>
-	bool operator>= (const List<T,Alloc>& lhs, const List<T,Alloc>& rhs)
+	bool operator>= (const Vector<T,Alloc>& lhs, const Vector<T,Alloc>& rhs)
 	{
 		return (!(lhs < rhs));
 	};
 
 	template <class T, class Alloc>
-	void swap (List<T,Alloc>& x, List<T,Alloc>& y)
+	void swap (Vector<T,Alloc>& x, Vector<T,Alloc>& y)
 	{
 		x.swap(y);
 	};
---
+++ Vector_main.cpp
@@ -0,0 +1,196 @@
+#include "Vector.hpp"
+#include <iostream>
+
+
+template <class T>
+void iter_print(ft::Vector<T> &vector)
+{
+	typedef typename ft::Vector<T>::iterator iter;
+	typedef typename ft::Vector<T>::reverse_iterator reverse_iter;
+
+	std::cout << "with iterator :";
+	for (iter it = vector.begin(); it != vector.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
+
+	std::cout << "with reverse_iterator :";
+	for (reverse_iter it = vector.rbegin(); it != vector.rend(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
+}
+
+
+int main (void)
+{
+	std::cout << "======constructor==========" << std::endl;
+	// constructors used in the same order as described above:
+
+	ft::Vector<int> first;                                // empty vector of ints
+	std::cout << "Empty : " << std::boolalpha << first.empty() << std::endl;
+	std::cout << "size : " << std::boolalpha << first.size() << std::endl;
+	ft::Vector<int> second ((unsigned int)4,100);                       // four ints with value 100
+	std::cout << "Empty : " << std::boolalpha << second.empty() << std::endl;
+	std::cout << "size : " << std::boolalpha << second.size() << std::endl;
+
+	iter_print(second);
+	ft::Vector<int> third(second.begin(),second.end());  // iterating through second
+	iter_print(third);
+	ft::Vector<int> fourth (third);                       // a copy of third
+	iter_print(fourth);
+	// the iterator constructor can also be used to construct from arrays:
+	int myints[] = {16,2,77,29};
+	ft::Vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+
+	std::cout << "The contents of fifth are:";
+	for (ft::Vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+	std::cout << ' ' << *it;
+	std::cout << '\n';
+	std::cout << "======resize & size & capcity ==========" << std::endl;
+
+	ft::Vector<int> myvector;
+	for (int i=1;i<10;i++)
+		myvector.push_back(i);
+	std::cout << "size: " << (int) myvector.size() << '\n';
+	std::cout << "capacity: " << (int) myvector.capacity() << '\n';
+	std::cout << "max_size: " << myvector.max_size() << '\n';
+
+	iter_print(myvector);
+	myvector.resize(5);
+	iter_print(myvector);
+	myvector.resize(8,100);
+	iter_print(myvector);
+	myvector.resize(12);
+	iter_print(myvector);
+
+	std::cout << "myvector contains:";
+	for (unsigned int i=0;i<myvector.size();i++)
+		std::cout << ' ' << myvector[i];
+	std::cout << '\n';
+
+	std::cout << "======reserve==========" << std::endl;
+	ft::Vector<int>::size_type sz;
+
+	ft::Vector<int> x;
+	sz = x.capacity();
+	std::cout << "making x grow:\n";
+	for (int i=0; i<100; ++i)
+	{
+		x.push_back(i);
+		if (sz != x.capacity())
+		{
+			sz = x.capacity();
+			std::cout << "capacity changed: " << sz << '\n';
+		}
+	}
+
+	ft::Vector<int> y;
+	sz = y.capacity();
+	y.reserve(100);   // this is the only difference with x above
+	std::cout << "making y grow:\n";
+	for (int i=0; i<100; ++i) {
+		y.push_back(i);
+		if (sz != y.capacity())
+		{
+			sz = y.capacity();
+			std::cout << "capacity changed: " << sz << '\n';
+		}
+    }
+	std::cout << "=======operator []=========" << std::endl;
+
+	ft::Vector<int> vec(10);
+	ft::Vector<int>::size_type sz2 = vec.size();
+	for (unsigned i=0; i < sz2; i++)
+		vec[i] = i;
+	for (unsigned i=0; i < sz2 / 2; i++)
+	{
+		int temp;
+		temp = vec[sz2-1-i];
+		vec[sz2-1-i] = vec[i];
+		vec[i]=temp;
+	}
+
+	std::cout << "myvector contains:";
+	for (unsigned i = 0; i < sz2; i++)
+		std::cout << ' ' << vec[i];
+	std::cout << '\n';
+
+	std::cout << "=======at=========" << std::endl;
+	for (unsigned i = 0 ; i< vec.size(); i++)
+    	vec.at(i) = i;
+	std::cout << "myvector contains:";
+	for (unsigned i = 0; i < sz2; i++)
+		std::cout << ' ' << vec.at(i);
+	std::cout << '\n';
+	try
+	{
+		vec.at(-1);
+	}
+	catch (std::exception const &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << "=======front & back & push_back & pop_back=========" << std::endl;
+	iter_print(vec);
+	std::cout << "front : " << vec.front() << std::endl;
+	std::cout << "back : " << vec.back() << std::endl;
+	vec.push_back(123);
+	iter_print(vec);
+	vec.pop_back();
+	iter_print(vec);
+
+
+
+	std::cout << "=======front & back & push_back & pop_back=========" << std::endl;
+
+	ft::Vector<int> vec1;
+	ft::Vector<int> vec2;
+	ft::Vector<int> vec3;
+
+	vec1.assign ((unsigned int)7,100);             // 7 ints with a value of 100
+	iter_print(vec1);
+	ft::Vector<int>::iterator it;
+	it = vec1.begin() + 1;
+
+	vec2.assign (it, vec1.end() - 1); // the 5 central values of first
+	iter_print(vec2);
+
+	int myints2[] = {1776,7,4};
+	vec3.assign (myints2 , myints2 + 3);   // assigning from array.
+	iter_print(vec3);
+
+	std::cout << "Size of first: " << int (vec1.size()) << '\n';
+	std::cout << "Size of second: " << int (vec2.size()) << '\n';
+	std::cout << "Size of third: " << int (vec3.size()) << '\n';
+
+	std::cout << "========erase=========" << std::endl;
+	iter_print(vec);
+	vec.erase(vec.begin() + 5);
+	iter_print(vec);
+	vec.erase(vec.begin(), vec.begin() + 3);
+	iter_print(vec);
+
+	// std::cout << "========swap=========" << std::endl;
+
+	// ft::Vector<int> a((unsigned int)3, 100);
+	// std::cout << "a: ";
+	// iter_print(a);
+	// ft::Vector<int> b((unsigned int)5, 200);
+	// a.swap(b);
+	// std::cout << "a: ";
+	// iter_print(a);
+
+	// std::cout << "========swap=========" << std::endl;
+
+	// ft::Vector<int> foo ((unsigned int)3,100);   // three ints with a value of 100
+	// ft::Vector<int> bar ((unsigned int)2,200);   // two ints with a value of 200
+
+	// if (foo==bar) std::cout << "foo and bar are equal\n";
+	// if (foo!=bar) std::cout << "foo and bar are not equal\n";
+	// if (foo< bar) std::cout << "foo is less than bar\n";
+	// if (foo> bar) std::cout << "foo is greater than bar\n";
+	// if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+	// if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+
+
+	return (0);
+}
