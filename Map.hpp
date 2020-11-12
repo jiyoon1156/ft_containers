984abeb80c23bb1e37d45f95ca520520cc693f59
Jiyoon Hur <jiyoon1156@gmail.com>
2020-11-12 15:45:22 +0900

    ADD: map file

---
+++ Map.hpp
@@ -0,0 +1,24 @@
+#ifndef MAP_HPP
+# define MAP_HPP
+
+#include <iostream>
+
+namespace ft
+{
+	template <class T>
+	class MapIterator: public iterator<bidrectional_iterator_tag, T>
+	{
+
+	};
+	template < class Key,                                     // map::key_type
+           class T,                                       // map::mapped_type
+           class Compare = std::less<Key>,                     // map::key_compare
+           class Alloc = std::allocator<std::pair<const Key,T> >    // map::allocator_type
+           >
+	class map
+	{
+
+	};
+}
+
+#endif
